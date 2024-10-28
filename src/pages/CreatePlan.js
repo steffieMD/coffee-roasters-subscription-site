27fdfffda2c0c05a1429eec80d7a8d07bef7cf26
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-10-28 11:12:12 +0100

    Update Home.js

--- src/pages/CreatePlan.js
+++ src/pages/CreatePlan.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const CreatePlan = () => {
-  return <div>Contact Page Content</div>;
+  return (
+    <div className="">
+      <div className="bg-[url('/src/assets/png/hero-img-subscribe.png')] text-[#FEFCF7] py-[100px] px-6 bg-cover text-center rounded-[10px] bg-center isolate brightness-50">
+        <h1 className="pb-6 font-fraunces text-[40px] font-[900] leading-10">
+          Great coffee made simple.
+        </h1>
+        <p className="font-barlow text-[15px] leading-[25px] pb-[39px]">
+          Start your mornings with the world’s best coffees. Try our expertly
+          curated artisan coffees from our best roasters delivered directly to
+          your door, at your schedule.
+        </p>
+        <button className="bg-[#0E8784] px-[31px] pt-[15px] pb-[16px] rounded-md font-fraunces text-lg font-[900]">
+          Create your plan
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePlan;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,17 @@ module.exports = {
     extend: {
       colors: {
         customlightgray: "rgba(254, 252, 247, 0.50)",
+        customeggwhite: "#fefcf7",
+        customdarkgrey: "#2C343E",
+        customgrayishblue: "#333D4B",
+        bluelagoon: "#0E8784",
       },
       fontFamily: {
         fraunces: ["Fraunces", "sans-serif"],
         barlow: ["Barlow", "sans-serif"],
+      },
+      isolation: {
+        isolate: "isolate",
       },
     },
   },
